ec4b1117819f4c1e24b9faea740b6b232f9b6707
kiyoshioba <kiyoshioba.jpn@gmail.com>
2019-08-13 15:55:57 +0900

    first-commit

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,19 @@
 class GroupsController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+    if current_group.update(group_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
 end
