090c2ac3b02922dfed803b0fe75d5616ebe6037c
kiyoshioba <kiyoshioba.jpn@gmail.com>
2019-08-17 22:53:28 +0900

    second-commit

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -32,8 +32,11 @@ rescue ActiveRecord::PendingMigrationError => e
 end
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
+  Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+  config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include ControllerMacros, type: :controller
+  config.include FactoryBot::Syntax::Methods
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
