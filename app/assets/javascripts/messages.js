1732f4834cca10a1fe85642daaa025e58f45f596
kiyoshioba <kiyoshioba.jpn@gmail.com>
2019-08-31 18:44:31 +0900

    comitt

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -34,10 +34,9 @@ $(function(){
       .done(function(data){
         var html = buildHTML(data);
         $('.messages').append(html);
-       $( ".form__submit").prop( "disabled", false );
-       $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
-       $('.form__message').val('');
-       $('.hidden').reset();
+        $('.new_message')[0].reset();
+        $( ".form__submit").prop( "disabled", false );
+        $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
       })
      .fail(function(){
         alert('error');
