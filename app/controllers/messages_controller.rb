21f239922bf6d51606f698966cea17426f5d45d3
kiyoshioba <kiyoshioba.jpn@gmail.com>
2019-08-31 16:10:55 +0900

    first-commit

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,6 +4,10 @@ class MessagesController < ApplicationController
   def index
     @message = Message.new
     @messages = @group.messages.includes(:user)
+      respond_to do |format|
+        format.html
+        format.json
+      end
   end
 
   def create
@@ -14,6 +18,7 @@ class MessagesController < ApplicationController
         format.json
       end
     else
+      @messages = @group.messages.includes(:user)
       flash.now[:alert] = 'メッセージを入力してください。'
       render :index
     end
