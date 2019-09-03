6db924e9b20c83e272af50582e8f588e56007a03
kiyoshioba <kiyoshioba.jpn@gmail.com>
2019-09-03 20:42:57 +0900

    自動更新機能

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -1,7 +1,7 @@
 $(function(){
     function buildHTML(message){
       var imagehtml = message.image == null ? "" : `<img src="${message.image}" class="lower-message__image">`
-      var html = `<div class=message>
+      var html = `<div class="message" data-message-id="${message.id}">
                       <div class="upper-message">
                         <div class="upper-message__user-name">
                         ${message.user_name}
@@ -19,6 +19,7 @@ $(function(){
                     </div> `
       return html;
     }
+
     $('#new_message').on('submit', function(e){
       e.preventDefault();
       var formData = new FormData(this);
@@ -42,4 +43,28 @@ $(function(){
         alert('error');
       })
     })
-  });
+
+    var reloadMessages = function () {
+      if (window.location.href.match(/\/groups\/\d+\/messages/)){
+        var last_message_id = $('.message:last').data("message-id");
+        $.ajax({
+          url: "api/messages",
+          type: 'get',
+          dataType: 'json',
+          data: {last_id: last_message_id}
+        })
+        .done(function (messages) {
+          var insertHTML = '';
+          messages.forEach(function (message) {
+            insertHTML = buildHTML(message);
+            $('.messages').append(insertHTML);
+          })
+          $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+        })
+        .fail(function () {
+          alert('自動更新に失敗しました');
+        });
+      }
+    };
+  setInterval(reloadMessages, 5000);
+});
